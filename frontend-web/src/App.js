2d205654ccbcf1cfeddc93ff4a30df5b6f99bd17
DivineDX <chia.dexun97@gmail.com>
2019-05-30 22:55:22 +0800

    Banner Page

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
-
+import BannerPage from './Components/BannerPage/BannerPage';
 class App extends Component{
 	render() {
 		return (
 			<div>
 				<NavBar/>
-				{/* <CoverPage/>
-				<Bulletin/> */}
+				<BannerPage/>
+				{/* <Bulletin/> */}
 			</div>
     	);
   }
