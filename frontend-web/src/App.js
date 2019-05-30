019cfeef8388213ec7f10564ac5f7445d241dace
DivineDX <chia.dexun97@gmail.com>
2019-05-30 23:48:31 +0800

    Added fake data

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -2,13 +2,21 @@ import React, {Component} from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import BannerPage from './Components/BannerPage/BannerPage';
+import Bulletin from './Container/Bulletin/Bulletin';
+import {Data} from './Data';
+
 class App extends Component{
+
+	componentDidMount() {
+		console.log(Data);
+	}
+
 	render() {
 		return (
 			<div>
 				<NavBar/>
 				<BannerPage/>
-				{/* <Bulletin/> */}
+				<Bulletin/>
 			</div>
     	);
   }
---
+++ frontend-web/src/Data.js
@@ -0,0 +1,22 @@
+export const Data = [
+    {
+        date_started: new Date(2019, 5, 30),
+        header: "Petition to Office of Student Affairs",
+        title: "Petition to install more recycling bins in hostels",
+        description: "I'm a year 2 student who is currently in PGP House, and this is my second year staying on campus. Ever since I moved in, I've realised that there isn't a lot of recycling bins. And it's quite a far walk from my room to the nearest recycling bin, which makes it very inconvenient for me. As someone who",
+        organizer: "Anonymous",
+        image: "IMG",
+        numSupporters: 201,
+        numLikes: 256,
+    },
+    {  
+        date_started: new Date(2019, 5, 20),
+        header: "Campaign for OCIP Project",
+        title: "OCIP Fundraising Project",
+        description: "I'm Xiao Ming, a year 3 Math major. My friends and I are planning to organise an OCIP project to Cambodia. We intend to help a local village to build a water filtration system, and we're looking for volunteers to help us. This is a voluntary project, so you will have to settle your own expenses.",
+        organizer: "Xiao Ming",
+        image: "IMG",
+        numSupporters: 150,
+        numLikes: 300,
+    }
+]
