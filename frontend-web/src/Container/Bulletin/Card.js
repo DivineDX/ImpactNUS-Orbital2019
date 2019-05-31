a946a686a6e8de56ed0371e4adb7fc0cfb3de2fb
DivineDX <chia.dexun97@gmail.com>
2019-06-01 00:08:56 +0800

    Slight improvements to container, will improve responsiveness tmr

--- frontend-web/src/Container/Bulletin/Card.js
+++ frontend-web/src/Container/Bulletin/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProgressBar from '../../Components/ProgressBar/ProgressBar';
+import './Card.css';
 
 const Card = ({ key, type, date_started, header, title, description, organizer, image, numSupporters, targetNum, numLikes }) => { //Destructuring right here
     let footer = '';
@@ -25,13 +26,13 @@ const Card = ({ key, type, date_started, header, title, description, organizer, 
         </div> */
 
         <article className="bt bb b--black-10">
-            <div className="baskerville db pv4 ph3 ph0-l black">
-                <div className="flex flex-column flex-row-ns">
-                    <div className="pr3-ns mb4 mb0-ns w-100 w-30-ns">
-                        <img src= {image} className="db h-auto"
+            <div className="baskerville pv4 ph3 ph0-l black">
+                <div className="container flex flex-column flex-row-ns">
+                    <div className="h-auto db pr3-ns mb4 mb0-ns w-100 w-25-ns">
+                        <img src= {image} className="h-auto"
                             alt="Uploaded IMG"/>
                     </div>
-                    <div className="w-100 w-70-ns pl3-ns">
+                    <div className=" w-100 w-70-ns pl3-ns">
                         <p className = "i">{header}</p>
                         <h1 className="f4 fw8 lh-title">{title}</h1>
                         <p className="f6 f5-l lh-copy">
