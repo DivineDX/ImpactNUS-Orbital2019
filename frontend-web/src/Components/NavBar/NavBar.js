46e6f21a23a109324b0b7b981aca4f7fed98c088
phaebaez <50545929+phaebaez@users.noreply.github.com>
2019-06-01 21:09:01 +0800

    made nav bar a sticky

--- frontend-web/src/Components/NavBar/NavBar.js
+++ frontend-web/src/Components/NavBar/NavBar.js
@@ -3,13 +3,12 @@ import "./NavBar.css";
 
 const NavBar = () => {
     return (
-        <nav className="black flex bb bg-red">
+        <nav id="BGColour">
             <a id = "Logo" className="link no-underline flex items-center pa2 br" href="#0">
             DIAGNUS
             </a>
             <div className = "flex items-center pa3 fw9">
-                <a className="link dib dim mr3 mr4-ns" href="#0">Petitions</a>
-                <a className="link dib dim mr3 mr4-ns" href="#0">Campaigns</a>
+                <a className="link dib dim mr3 mr4-ns" href="#0">Bulletin</a>
             </div>
             <div className="wrap">
                 <div className="search">
