e85db3a5bf4180bc124e233d3eab90e5ceb2a3c1
DivineDX <chia.dexun97@gmail.com>
2019-06-04 10:28:17 +0800

    Responsive Main Page

--- frontend-web/src/Container/Bulletin/Card.js
+++ frontend-web/src/Container/Bulletin/Card.js
@@ -11,27 +11,48 @@ const Card = ({ key, type, date_started, header, title, description, organizer, 
     }
 
     return (
-        <section class="ph7-ns ph3-m center avenir">
-            <article class="bt bb b--black-10">
-                <a class="db pv4 ph3 ph0-l no-underline black" href="#0">
-                    <div class="container flex flex-column-m flex-row-ns">
-                        <div class="pr3-ns mb4 mb0-ns w-100 w-30-ns">
-                            <img src={image} class="db h-100 w-100" alt="IMG" />
+        // <section class="ph7-ns ph3-m center avenir">
+        //     <article class="bt bb b--black-10">
+        //         <a class="db pv4 ph3 ph0-l no-underline black" href="#0">
+        //             <div class="container flex flex-column-m flex-row-ns">
+        //                 <div class="pr3-ns mb4 mb0-ns w-100 w-30-ns">
+        //                     <img src={image} class="db h-100 w-100" alt="IMG" />
+        //                 </div>
+        //                 <div className="w-100 w-70-ns pl3-ns">
+        //                     <p className="i">{header}</p>
+        //                     <h1 className="f4 fw8 lh-title">{title}</h1>
+        //                     <p className="f6 f5-l lh-copy">
+        //                         {description}
+        //                     </p>
+        //                     <p className="f6 fw8 lh-copy mv0">
+        //                         By {organizer}
+        //                     </p>
+        //                     <ProgressBar numSupporters = {numSupporters} targetNum = {targetNum}/>
+        //                     <p>{footer}</p>
+        //                 </div>
+        //             </div>
+        //         </a>
+        //     </article>
+        // </section>
+        <section className="avenir">
+            <article className="bt bb b--black-10">
+                    <div className="card-container">
+                        <div className="img_wrap">
+                            <img src={image} className="w-100" alt="IMG" />
                         </div>
-                        <div className="w-100 w-70-ns pl3-ns">
+                        <div className="text_wrap">
+                            <h1 className="">{title}</h1>
                             <p className="i">{header}</p>
-                            <h1 className="f4 fw8 lh-title">{title}</h1>
-                            <p className="f6 f5-l lh-copy">
+                            <p className="">
                                 {description}
                             </p>
-                            <p className="f6 fw8 lh-copy mv0">
+                            <p className="b">
                                 By {organizer}
                             </p>
                             <ProgressBar numSupporters = {numSupporters} targetNum = {targetNum}/>
                             <p>{footer}</p>
                         </div>
                     </div>
-                </a>
             </article>
         </section>
     )
