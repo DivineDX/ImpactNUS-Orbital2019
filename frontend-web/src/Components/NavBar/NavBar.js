e395b0e4e42f6af0f5ab4b5c48f2579a2bbbdb9e
phaebaez <50545929+phaebaez@users.noreply.github.com>
2019-06-16 22:54:16 +0800

    basic layout of header edited

--- frontend-web/src/Components/NavBar/NavBar.js
+++ frontend-web/src/Components/NavBar/NavBar.js
@@ -1,48 +1,51 @@
-import React from 'react';
-import "./NavBar.css";
-import LoginModal from "../LoginModal/LoginModal";
-import { Button } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Button, Input, Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import "./NavBar.css";
 
-const NavBar = ({ loginProp, isSignedIn }) => {    
+class NavBar extends Component {
+  state = { activeItem: 'DiagNus' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
     return (
-        <div className="bb menu">
+    <Menu stackable>
+        <Menu.Item>
             <Link to="/" className="link no-underline flex items-center pa2 br">
                 <div id="Logo">
                     DIAGNUS
                 </div>
             </Link>
-            <div className="flex items-center pa3 fw9">
-                <Link to="/bulletin">
-                    <div className="link dib dim mr3 mr4-ns">Bulletin</div>
-                </Link>
-                <Link to="/feed">
-                    <div className="link dib dim mr3 mr4-ns">My Feed</div>
-                </Link>
-                <Link to="/dashboard">
-                    <div className="link dib dim mr3 mr4-ns">My Dashboard</div>
-                </Link>
-
-            </div>
-            <div className="right item wrap">
-                <div className="ui action input search">
-                    <input type="text" placeholder="Search..." />
-                    <button type="submit" className="ui button go">Go</button>
-                </div>
-            </div>
-            <div className="flex-grow pa3 flex items-center ml7">
-                <div id="Login">
-                    {isSignedIn === false //conditional
-                        ? <LoginModal loginProp={loginProp} buttonWord={"Login"} /> //loginUser function
-                        : <Button onClick={() => loginProp()}>Sign Out</Button> //onClick, will run the signOutUser as defined in App.js
-                    }
-                </div>
-            </div>
-        </div>
-    );
+        </Menu.Item>
+        <Menu.Item
+          name='Bulletin'
+          active={activeItem === 'Bulletin'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='My Feed'
+          active={activeItem === 'My Feed'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='My Dashboard'
+          active={activeItem === 'My Dashboard'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Menu position='right'>
+            <Menu.Item position='right'>
+                <Input action={{ type: 'submit', content: 'Go' }} placeholder='Search...' />
+            </Menu.Item>
+          <Menu.Item>
+            <Button secondary> Login </Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    )
+  }
 }
 
-
 export default NavBar;
-
-
