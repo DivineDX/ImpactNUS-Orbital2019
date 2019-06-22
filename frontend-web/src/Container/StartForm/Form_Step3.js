caab09a2be1f8818bd6d0b28b9bc3dab6d114f18
DivineDX <chia.dexun97@gmail.com>
2019-06-22 18:19:02 +0800

    Fixed form bugs, 80% working functionality, but not responsive

--- frontend-web/src/Container/StartForm/Form_Step3.js
+++ frontend-web/src/Container/StartForm/Form_Step3.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import { Button, TextArea, Form, Dropdown } from 'semantic-ui-react'
+import { Button, TextArea, Form, Dropdown, Input } from 'semantic-ui-react'
+import tagOptions from './tagOptions';
 
-const tagOptions = [
-    {key: 'ac', value: 'ac', text: 'Academic'},
-    {key: 'cip', value: 'cip', text: 'CIP'},
-    {key: 'sl', value: 'sl', text: 'Student Life'},
-    {key: 'cl', value: 'cl', text: 'Campus Living'},
-];
 
-const Form_Step3 = ({ navButton, inputChange, currType }) => {
+const Form_Step3 = ({ navButton, inputChange, dropdownChange, currState }) => {
     return (
         <Form size='huge'>
             <Form.Field>
-                <h1>Provide a detailed description of your {currType}</h1>
-                <TextArea onChange={(event) => inputChange(event, 'description')} style={{ minHeight: 200 }} />
+                <h1>Provide a detailed description of your {currState.type}</h1>
+                <TextArea focus value = {currState.description} onChange={(event) => inputChange(event, 'description')} style={{ minHeight: 200 }} />
             </Form.Field>
 
             <Form.Field>
-                <h1>Add a photo</h1>
-                <input onChange={(event) => inputChange(event, 'imageURL')} placeholder="Link to an image hosting site" />
+                <h1>Add a photo (Optional but highly recommended)</h1>
+                <Input focus value = {currState.imageURL} onChange={(event) => inputChange(event, 'imageURL')} placeholder="Link to an image hosting site" />
             </Form.Field>
 
             <Form.Field>
                 <h1>Input relevant tags</h1>
-                <Dropdown clearable fluid multiple search selection options={tagOptions} placeholder='Insert tags'/>
+                <Dropdown 
+                    clearable fluid multiple search selection
+                    value = {currState.tags} 
+                    options={tagOptions} 
+                    onChange={dropdownChange.bind(this)}
+                    placeholder='Insert tags'/>
             </Form.Field>
 
             <Button.Group>
                 <Button labelPosition='left' icon='left chevron' onClick={() => navButton(2)} content='Previous' />
-                <Button labelPosition='right' icon='right chevron' onClick={() => navButton(4)} content='Next' />
+                <Button 
+                    disabled = {currState.description === ''}
+                    labelPosition='right' icon='right chevron' onClick={() => navButton(4)} content='Next' />
             </Button.Group>
         </Form>
     );
--- frontend-web/src/Container/StartForm/StartForm.js
+++ frontend-web/src/Container/StartForm/StartForm.js
@@ -4,18 +4,19 @@ import FormStep1 from './Form_Step1';
 import FormStep2 from './Form_Step2';
 import FormStep3 from './Form_Step3';
 import FormStep4 from './Form_Step4';
+import './StartForm.css';
 
 const defaultState = {
     currentStep: 1,
-        type: '',
-        title: '',
-        targetGroup: '',
-        // endDate: '',
-        targetSupporters: '',
-        anonymity: false,
-        tags: [],
-        description: '',
-        imageURL: ''
+    type: '',
+    title: '',
+    targetGroup: '',
+    // endDate: '',
+    targetSupporters: '',
+    anonymity: false,
+    tags: [],
+    description: '',
+    imageURL: ''
 }
 
 class StartForm extends Component {
@@ -26,7 +27,7 @@ class StartForm extends Component {
             type: '',
             title: '',
             targetGroup: '',
-            // endDate: '',
+            endDate: '',
             targetSupporters: '',
             anonymity: false,
             tags: [],
@@ -41,96 +42,108 @@ class StartForm extends Component {
         4: Preview and Confirmation
         */
     }
-    
+
     toggleType = (type) => {
-        if(type === 'petition') {
-            this.setState({type: 'petition'});
-        }else if(type === 'campaign') {
-            this.setState({type: 'campaign'});
+        if (type === 'petition') {
+            this.setState({ type: 'petition' });
+        } else if (type === 'campaign') {
+            this.setState({ type: 'campaign' });
         }
     }
 
     toggleAnonymity = () => {
-        if(this.state.anonymity) { //true
-            this.setState({anonymity: false});
-        }else {
-            this.setState({anonymity: true});
+        if (this.state.anonymity) { //true
+            this.setState({ anonymity: false });
+        } else {
+            this.setState({ anonymity: true });
         }
     }
 
     onInputChange = (event, category) => {
-        console.log("input Change", category);
-        switch(category) {
+        // console.log("input Change", category);
+        switch (category) {
             case "title":
-                this.setState({title : event.target.value}); //petition or campaign 
+                this.setState({ title: event.target.value }); //petition or campaign 
                 break;
             case "targetGroup":
-                this.setState({targetGroup : event.target.value}); //petition or campaign 
+                this.setState({ targetGroup: event.target.value }); //petition or campaign 
                 break;
             case "targetSupporters":
-                this.setState({targetSupporters : event.target.value}); //petition or campaign 
+                this.setState({ targetSupporters: event.target.value }); //petition or campaign 
                 break;
-            case "tags":
-                this.setState({title : event.target.value}); //petition or campaign 
+            case "date":
+                console.log("Date change");
+                console.log(event.target.value);
+                this.setState({ endDate: event.target.value }); //petition or campaign 
                 break;
             case "description":
-                this.setState({description : event.target.value}); //petition or campaign 
+                this.setState({ description: event.target.value }); //petition or campaign 
                 break;
             case "imageURL":
-                this.setState({imageURL : event.target.value}); //petition or campaign 
+                this.setState({ imageURL: event.target.value }); //petition or campaign 
                 break;
+
             default:
                 break;
         }
+    }
+
+    onDropdownChange = (e, { value }) => {
+        // console.log(value);
+        this.setState({ tags: value });
     }
 
     currentStep = (stepNumber) => {
-        switch(stepNumber) {
+        switch (stepNumber) {
             case 1:
-                this.setState({currentStep: 1});
+                this.setState({ currentStep: 1 });
                 break;
             case 2:
-                this.setState({currentStep: 2});
+                this.setState({ currentStep: 2 });
                 break;
             case 3:
-                this.setState({currentStep: 3});
+                this.setState({ currentStep: 3 });
                 break;
             case 4:
-                this.setState({currentStep: 4});
+                this.setState({ currentStep: 4 });
                 break;
             default:
-                this.setState({currentStep: 1});
+                this.setState({ currentStep: 1 });
                 break;
         }
     }
 
-    render() {        
+    render() {
         return (	 //acts as a card list here
-            <div className='flex flex-column items-center'>
-                <MultistepMenu currentStep = {this.state.currentStep}/>
-                <div className='w-65'>
-                    {this.state.currentStep === 1 && 
-                        <FormStep1 
-                            navButton = {this.currentStep} 
-                            toggleType = {this.toggleType}
-                            inputChange = {this.onInputChange}
-                            currType = {this.state.type}/>}
-                    {this.state.currentStep === 2 && 
-                        <FormStep2 
-                            navButton = {this.currentStep}
-                            inputChange = {this.onInputChange}
-                            toggleAnonymity = {this.toggleAnonymity}
-                            currentAnonymity = {this.state.anonymity}
-                            currType = {this.state.type}/>}
-                    {this.state.currentStep === 3 && 
-                        <FormStep3 
-                            navButton = {this.currentStep}
-                            inputChange = {this.onInputChange}
-                            currType = {this.state.type}/>}
-                    {this.state.currentStep === 4 && 
+            <div id="formContainer" className = "flex flex-column items-center mt4 mb4">
+                <div id = "stepContainer" className = "pb3">
+                    <MultistepMenu currentStep={this.state.currentStep} />
+                </div>
+                
+                <div id="inputContainer">
+                    {this.state.currentStep === 1 &&
+                        <FormStep1
+                            navButton={this.currentStep}
+                            toggleType={this.toggleType}
+                            inputChange={this.onInputChange}
+                            currState={this.state} />}
+                    {this.state.currentStep === 2 &&
+                        <FormStep2
+                            navButton={this.currentStep}
+                            inputChange={this.onInputChange}
+                            toggleAnonymity={this.toggleAnonymity}
+                            currentAnonymity={this.state.anonymity}
+                            currState={this.state} />}
+                    {this.state.currentStep === 3 &&
+                        <FormStep3
+                            navButton={this.currentStep}
+                            inputChange={this.onInputChange}
+                            dropdownChange={this.onDropdownChange}
+                            currState={this.state} />}
+                    {this.state.currentStep === 4 &&
                         <FormStep4
-                            navButton = {this.currentStep}
-                            currType = {this.state.type}/>}
+                            navButton={this.currentStep}
+                            currState={this.state} />}
                 </div>
             </div>
         );
