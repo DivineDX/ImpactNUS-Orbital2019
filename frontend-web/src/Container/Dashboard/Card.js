2c24087c9006567a6eaf675efccdef8bfc47b721
phaebaez <50545929+phaebaez@users.noreply.github.com>
2019-06-23 16:10:07 +0800

    fixed follow button:

---
+++ frontend-web/src/Container/Dashboard/Card.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ProgressBar from '../../Components/ProgressBar/ProgressBar';
+import Follow from './Follow';
+import { Link } from 'react-router-dom';
+
+const Card = ({ key, type, title, recipient, organizer, anonymity, date_started, description, image, targetNum, numSupporters, numFollowing }) => { //Destructuring
+    let footer = '';
+    if (type === 'petition') {
+        footer = numSupporters + " Signed | Target: " + targetNum;
+    } else { //campaign
+        footer = numSupporters + " Supported | Target: " + targetNum;
+    }
+
+    let displayedOrganizer = '';
+    if (anonymity) {
+        displayedOrganizer = "Anonymous";
+    } else{
+        displayedOrganizer = organizer;
+    }
+
+    let flag = false;
+
+    return (
+        <section className="avenir">
+            <article className="bb b--black-10">
+                    <div className="card-container">
+                        <div className="img_wrap">
+                            <img src={image} className="w-100" alt="IMG" />
+                        </div>
+                        <div className="text_wrap">
+                            <h1 className="">{title} </h1>
+                            <Follow>
+                                Follow
+                            </Follow>
+                            <p className="i">{recipient}</p>
+                            <p className="">
+                                {description}
+                                <text> </text>
+                                <Link to="/"> 
+                                {/* to add links to indiv page  */}
+                                <text className='readmore'> Read more </text>
+                                </Link>
+                            </p>
+                            <ProgressBar numSupporters = {numSupporters} targetNum = {targetNum}/>
+                            <p>{footer}</p>
+                        </div>
+                    </div>
+            </article>
+        </section>
+    )
+}
+
+export default Card;
