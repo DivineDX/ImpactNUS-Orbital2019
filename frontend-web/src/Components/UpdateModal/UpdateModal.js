0dc5a92d2c549ef263eee91f17128c3984a145ac
phaebaez <50545929+phaebaez@users.noreply.github.com>
2019-06-30 16:36:15 +0800

    hope I modified the update modal correctly

--- frontend-web/src/Components/UpdateModal/UpdateModal.js
+++ frontend-web/src/Components/UpdateModal/UpdateModal.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
-import { Dropdown, Modal, TextArea, Form, Button } from 'semantic-ui-react'
+import { Dropdown, Modal, Form, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 
 class UpdateModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            updateTitle: '',
+            updateDescription: '',
+        }
+    }
+    
+    onUpdate = () => { //modify this after database is coded
+        fetch('http://localhost:3001/update', {
+            method: 'post',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                updateTitle: this.state.updateTitle,
+                updateDescription: this.state.updateDescription,
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            // this.resetDefault();
+            this.setState({updating: true});
+            console.log(data); //Object Data of the created petition/campaign
+    })
+    }
+
+    onInputChange = (event, category) => {
+        // console.log("input Change", category);
+        switch (category) {
+            case "updateTitle":
+                this.setState({ updateTitle: event.target.value }); //petition or campaign 
+                break;
+            case "description":
+                this.setState({ updateDescription: event.target.value }); //petition or campaign 
+                break;
+        }
+    }
+
     render() {
-        const { inputChange, currState, buttonWord, title } = this.props;
+        const { currState, buttonWord, title } = this.props;
 
         return (
             <Modal trigger={<Dropdown.Item className = 'hoverLink'>{buttonWord}</Dropdown.Item>}>
@@ -13,14 +51,16 @@ class UpdateModal extends Component {
                     <Form size='huge'>
                         <Form.Field>
                             <h1> Title {title} </h1>
+                            <input type = 'text' placeholder = 'Title your update' onChange={(event) => this.onInputChange(event, 'updateTitle')}/>
                         </Form.Field>
                         <Form.Field>
                             <h1> Update </h1>
-                            <TextArea
-                                // focus value = {currState.description} 
-                                onChange={(event) => inputChange(event, 'description')} style={{ minHeight: 200 }} />
+                            <textarea placeholder = "Updates" 
+                            onChange={(event) => this.onInputChange(event, 'updateDescription')} style={{ minHeight: 200 }} />
                         </Form.Field>
-                        <Button labelPosition='right' icon='right chevron' content='Update' />
+                        <Link to="/">
+                            <Button labelPosition='right' icon='right chevron' content='Update'/>
+                        </Link>
                     </Form>
                 </Modal.Content>
             </Modal>
