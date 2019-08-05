29bcfd2af0ae2d407ec7a8e478b1f955e9dcc9af
DivineDX <chia.dexun97@gmail.com>
2019-08-05 12:05:34 +0800

    Fixed bugs

--- backend-server/config/urls.js
+++ backend-server/config/urls.js
@@ -1,5 +1,5 @@
-const apiServerURL = 'localhost:3001'; //for local hosting
-// const apiServerURL = 'impactnus-api.herokuapp.com';
+// const apiServerURL = 'localhost:3001'; //for local hosting
+const apiServerURL = 'impactnus-api.herokuapp.com';
 const frontendURL = 'http://localhost:3000';
 
 module.exports = {
